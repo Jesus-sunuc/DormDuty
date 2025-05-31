8498b08097e6fdf3b53d73e39389cbd37ab2905d
Jesus Sunuc <mariano.code4guate@gmail.com>
2025-05-31 16:02:04 -0600

    updating info

--- config/firebase.template.ts
+++ config/firebase.template.ts
@@ -1,10 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import {initializeAuth, getReactNativePersistence} from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
