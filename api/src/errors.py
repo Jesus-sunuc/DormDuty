1e7ebadfa005a69eddba3edc4f9889ea32bb71ce
Jesus Sunuc <mariano.code4guate@gmail.com>
2025-06-14 12:03:23 -0600

    full error handler is back

--- api/src/errors.py
+++ api/src/errors.py
@@ -1,37 +1,40 @@
 from functools import wraps
 import pprint
 import traceback
-import inspect
 
 from fastapi import HTTPException
 
+
 def error_handler(error_message: str):
     def decorator(func):
-        if inspect.iscoroutinefunction(func):
-            @wraps(func)
-            async def async_wrapper(*args, **kwargs):
-                try:
-                    return await func(*args, **kwargs)
-                except Exception as e:
-                    print(f"\nError in async function '{func.__name__}':")
-                    print("Arguments (args):")
-                    pprint.pprint(args)
-                    pprint.pprint(f"{error_message}: {e}")
-                    traceback.print_exc()
-                    raise HTTPException(status_code=500, detail=error_message)
-            return async_wrapper
-        else:
-            @wraps(func)
-            def sync_wrapper(*args, **kwargs):
-                try:
-                    return func(*args, **kwargs)
-                except Exception as e:
-                    print(f"\nError in sync function '{func.__name__}':")
-                    print("Arguments (args):")
-                    pprint.pprint(args)
-                    pprint.pprint(f"{error_message}: {e}")
-                    traceback.print_exc()
-                    raise HTTPException(status_code=500, detail=error_message)
-            return sync_wrapper
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except ValueError as e:
+                print(f"\nError in function '{func.__name__}':")
+                print("Arguments (args):")
+                pprint.pprint(args)
+                pprint.pprint(f"{error_message}: {e}")
+                traceback.print_exc()
+                raise HTTPException(status_code=500, detail=f"{error_message}")
+            except HTTPException as e:
+                print(f"\nError in function '{func.__name__}':")
+                print("Arguments (args):")
+                pprint.pprint(args)
+                pprint.pprint(f"{error_message}: {e}")
+                traceback.print_exc()
+                raise HTTPException(
+                    status_code=500, detail=f"{error_message}"
+                )
+            except Exception as e:
+                print(f"\nError in function '{func.__name__}':")
+                print("Arguments (args):")
+                pprint.pprint(args)
+                pprint.pprint(f"{error_message}: {e}")
+                traceback.print_exc()
+                raise HTTPException(status_code=500, detail=f"{error_message}")
+
+        return wrapper
 
     return decorator
